4078dfe8e4ad187749a8c038191a146c256210a1
Sean <sean@SeandeMacBook-Pro.local>
2026-02-13 03:22:18 +0800

    feat: memo

--- expamples/_components/MemoComponent.tsx
+++ expamples/_components/MemoComponent.tsx
@@ -1,9 +1,14 @@
-import { useReducer, useState } from "../which-react";
+import { useReducer, useState, useCallback } from "../which-react";
 import MySlowList from "./MemoComp-Child";
 
 function MemoComponent() {
   const [count, setCount] = useReducer((x) => x + 1, 0);
   const [text, setText] = useState("hello");
+
+  const func = useCallback(() => {
+    console.log("func");
+  }, []);
+
   return (
     <div className="border">
       <h1>函数组件</h1>
@@ -22,7 +27,7 @@ function MemoComponent() {
       />
       <p>{text}</p>
       {/* 非紧急更新 */}
-      <MySlowList text={text} />
+      <MySlowList text={text} func={func} />
     </div>
   );
 }
